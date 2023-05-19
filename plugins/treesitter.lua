dbf782fb5851e4b7a72a8def971782ab9db0b828
Patrick Raedler <patoraedler@gmail.com>
2023-05-19 23:01:45 +0200

    feat(treesitter): add ensure_installed

---
+++ plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "css",
+      "graphql",
+      "html",
+      "graphql",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "scss",
+      "tsx",
+      "typescript",
+      "vim",
+      "vue",
+      "yaml",
+    }
+  }
+}
