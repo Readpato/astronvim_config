0c440c220c211750b20e641da0c3fc6c786a68f6
Patrick Raedler <patoraedler@gmail.com>
2023-05-21 22:04:21 +0200

    chore(community): add surround

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,5 +7,6 @@ return {
   -- Editing Support
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   -- Motions
-  { import = "astrocommunity.motion.leap-nvim" }
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.mini-surround" }
 }
