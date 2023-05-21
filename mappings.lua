c6503439871c5cad39454ace516a9556ff703197
Patrick Raedler <patoraedler@gmail.com>
2023-05-21 21:48:01 +0200

    chore(mappings): add TODOs with trouble

--- mappings.lua
+++ mappings.lua
@@ -1,6 +1,11 @@
 return {
   n = {
+    -- Trouble
+    ["<leader>xt"] = { "<cmd>TodoQuickFix<cr>", desc = "Show all TODOs in project" },
+    -- Movement
     ["<C-d>"] = { "<C-d>zz", desc = "Jump half a screen down and center" },
-    ["<C-u>"] = { "<C-u>zz", desc = "Jump half a screen up and center" }
-  }
+    ["<C-u>"] = { "<C-u>zz", desc = "Jump half a screen up and center" },
+
+  },
+
 }
