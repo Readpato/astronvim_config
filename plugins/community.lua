3e2913b3c3559eea333b5c347c803987b8950c60
Patrick Raedler <patoraedler@gmail.com>
2023-06-23 16:22:00 +0200

    chore(plugins): add more colorschemes

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,6 +2,22 @@ return {
   "AstroNvim/astrocommunity",
   -- Colorschemes
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+    "catppuccin",
+    opts = {
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        }
+      }
+    },
+  },
   -- Diagnostics
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   -- Editing Support
